c39412b8e1ff305e22ada0b8282ef80bb671bce9
Anshul Singh Chauhan <151395342+Anshul-exe@users.noreply.github.com>
2024-03-23 21:05:15 +0530

    Delete first.js

--- first.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello World");
-console.log("testing");
-//below line won't work untill it's been run on website so if you want run this code here need to remove --> alert("yoi") <--
-alert("yoi");
